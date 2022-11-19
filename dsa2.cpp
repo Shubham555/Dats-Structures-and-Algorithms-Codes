bdd6091bb1faaeca473ed178d03a29b48cb10bb2
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-19 22:56:53 +0530

    Add files via upload

---
+++ dsa2.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*  int a;
+    cout<<"enter the value of a "<<endl;
+    cin>>a;
+
+    if(a>0){
+        cout<<"a is positive"<<endl;
+    }
+    else{
+        cout<<"a is negative "<<endl;
+    }  
+   int a,b;
+   cout<<"Enter the value of a and b"<<endl;
+   cin>>a>>b;
+
+   if(a>b){
+       cout<<"a is greater than b "<<endl;
+   }
+   if(b>a){
+       cout<<"b is greater than a "<<endl;
+   } 
+
+
+
+    int a;
+    cout << "enter the value of a " << endl;
+    cin >> a;
+    if (a > 0)
+    {
+        cout << "a is positive" << endl;
+    }
+    else if(a<0)
+    {
+        cout << "a is negative " << endl;
+    }
+    else{
+        cout<<"a is zero "<<endl;
+    }  
+
+  // print numbers from 1 to n.
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+
+    int i=1;
+
+    while(i<=n){
+        cout<<i<<endl;
+      i= i+1; // or i++;
+    }  
+
+    // print sum till the given numbers n
+
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+
+    int  sum=0;
+    int i =1;
+
+    while(i<=n){
+        sum=sum+i;
+        i=i+1;
+    }
+    cout<<sum<<endl;
+   // for sum all even numbers till n;
+   /* int  sum=0;
+    int i =2;
+
+    while(i<=n){
+        sum=sum+i;
+        i=i+2;
+    }
+    cout<<sum<<endl; 
+
+
+// prime or not
+   int n;
+   cin>>n;
+   int i=2;
+   
+   while(i<n){
+       if(n%i==0){
+           cout<<"not prime for "<<i<<endl;
+       }
+       else{
+           cout<<"prime for "<<i<<endl;
+       }
+       i=i+1;
+   }
+   */
+
+
+}

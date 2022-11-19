27409b65a53689da4bc2d0e827da8c3f65d574cf
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-19 22:55:32 +0530

    Add files via upload

---
+++ dsa1.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+   /* cout<<"Namaste duniya"<<endl;
+
+    int a = 4;
+    cout<< a <<endl;
+
+     char b = 'v';
+    cout<< b <<endl;
+
+    float f =1.2;
+    cout<< f <<endl;
+
+    bool bl = true;
+    cout<< bl <<endl;
+
+    double d = 1.234;
+    cout<< d <<endl;
+
+    int size = sizeof(a);
+    cout<<"size of a is "<<size<<endl;  
+
+   // type casting 
+   int a = 'a';
+   cout<<a<<endl;
+
+   char c = 98;
+   cout<<c<<endl;
+
+   char ch1 = 123456;
+   cout<<ch1<<endl; 
+
+   unsigned int a= 112; // unsigned,it prints only positive numbers no -112 
+   cout<<a<<endl;  
+
+//Arithmetic operator 
+
+   float a = 2.0/5;
+   cout<<a<<endl;
+   cout<<2.0/5<<endl; 
+   
+
+  //relational operator 
+
+  int a =2;
+  int b=3;
+  bool first = (a==b);
+  cout<<first<<endl;
+
+  bool second = (a>b);
+  cout<<second<<endl;
+
+  bool third = (a<b);
+  cout<<third<<endl;
+
+  bool fourth = (a>=b);
+  cout<<fourth<<endl;
+
+  bool fifth = (a<=b);
+  cout<<fifth<<endl;
+  
+  bool sixth = (a!=b);
+  cout<<sixth<<endl;
+  
+// Logical operators 
+   int a = 21;
+   cout<<!a<<endl;
+   */
+   }
+   

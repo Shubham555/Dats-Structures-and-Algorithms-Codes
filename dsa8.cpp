b4329da595a5716e708e06c4f642b39494df797b
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2022-11-24 20:16:00 +0530

    Add files via upload

---
+++ dsa8.cpp
@@ -0,0 +1,68 @@
+/****************  Switch Statement & Functions ******************************/
+/*switch case statement: 
+              js if else ch tsch thod diffrent
+            switch madhe dileli jashi case ahe tashi case ( bhetli ki ) khalchya kontya be case la match
+            match zali ki te execute/print karaych.  :)
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+  char ch ='1';
+  int num = 2;
+  cout<<endl;
+
+switch(3*num){
+
+    case 6:cout<<"first"<<endl;
+           cout<<"first again"<<endl;
+          break;
+        //break;
+      
+    case '1': switch(num){
+             case 1: cout<<"the value of num is "<<num<<endl;
+             break;
+            }
+            break;
+      
+    default:cout<<"It is defualt case"<<endl;
+            break;   
+  }
+   cout<<endl;
+
+
+}
+/*
+int main(){
+    int a,b;
+    cout<<"Enter the value of a "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b "<<endl;
+    cin>>b;
+
+    char op;
+    cout<<"Enter the operation you want to perform "<<endl;
+    cin>>op;
+
+    switch(op){
+    
+    case '+': cout<< (a+b) <<endl;
+            break;
+
+    case '-': cout<< (a-b) <<endl;
+            break;
+
+    case '*':cout<< (a*b) <<endl;
+            break;
+
+    case '/':cout<< (a/b) <<endl;
+            break;
+    case '%':cout<< (a%b) <<endl;
+            break;
+
+    default: cout<<"Enter the valid operation "<<endl; 
+
+
+    }
+
+}*/
